7c39bf1c266b17cdaf27bc62a9a2db8846e832a6
greatraksin <greatraksin@icloud.com>
2023-03-24 15:22:05 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Demid Raksin on 24.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
